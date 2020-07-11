fb815b5b8f2c82a106962319fc3bd6499969add9
sei-m <seiichimori@i.softbank.jp>
2020-07-11 22:58:08 +0900

    Initial Commit

---
+++ timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timer
+//
+//  Created by seimo on 2020/07/11.
+//  Copyright © 2020 test.seimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
